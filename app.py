d8d9683512e5442f125ad5efdb451c43d6717bc1
Julian Römer <julian_romer@orange.fr>
2021-03-15 15:48:33 +0100

    ...

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
-
 from flask import render_template
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=("static/templates"))
 
 
-@app.route("/")
+@ app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/About")
+@ app.route("/About")
 def about():
     return render_template("About.html")
 
 
-@app.route("/kitchen")
+@ app.route("/kitchen")
 def kitchen():
     return render_template("kitchen-compartment.html")
 
 
-@app.route("/left-wing")
+@ app.route("/left-wing")
 def left():
     return render_template("left-wing.html")
 
 
-@app.route("/office")
+@ app.route("/office")
 def office():
     return render_template("office.html")
 
 
-@app.route("/right-wing")
+@ app.route("/right-wing")
 def right():
     return render_template("right-wing.html")
 
