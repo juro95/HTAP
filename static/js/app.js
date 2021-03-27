30618fe2173402f13292e23f93c597cfa8c9e871
Julian Römer <julian_romer@orange.fr>
2021-03-27 13:54:52 +0100

    does login work ?

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,17 @@
+function onSignIn(googleUser) {
+    let profile = googleUser.getBasicProfile();
+    $(".g-signin2").css("display", "none");
+    $(".signOut").css("display", "block");
 
+}
+
+function signOut() {
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert("you have been signed out!")
+        $(".g-signin2").css("display", "block");
+        $(".signOut").css("display", "none");
+    })
+}
 
 
