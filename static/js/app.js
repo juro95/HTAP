74a3802d53ddaa5cd1bc5bb72caf5e97e6998422
Julian Römer <julian_romer@orange.fr>
2021-03-29 12:50:42 +0200

    solved login button

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,20 @@
-let login = document.querySelector(".g-signin2");
-let logout = document.querySelector(".signOut");
+window.onload = (event) => {
+    gapi.load('auth2', function () {
+        gapi.auth2.init();
+        console.log("im loaded")
+    });
+}
+
+
+// Sign in the user upon button click.
+function handleAuthClick(event) {
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signIn().then(function () {
+        alert("you have been signed in!")
+        login.style.display = "none";
+        logout.style.display = "block";
+    })
+}
 
 
 function onSignIn(googleUser) {
