7a93927a60eecbe7d9a9163d9f993e4aaedbe420
Julian Römer <julian_romer@orange.fr>
2021-03-30 17:53:28 +0200

    event?

--- static/js/app.js
+++ static/js/app.js
@@ -125,7 +125,7 @@ var event = {
     'location': '800 Howard St., San Francisco, CA 94103',
     'description': 'A chance to hear more about Google\'s developer products.',
     'start': {
-        'dateTime': '2015-05-28T09:00:00-07:00',
+        'dateTime': '2021-05-28T09:00:00-07:00',
         'timeZone': 'America/Los_Angeles'
     },
     'end': {
