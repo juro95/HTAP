54932ea5395a29d393a07ad2821ed53b6545914a
omarb <omar.badawy@hotmail.com>
2021-04-12 11:44:12 +0200

    new design

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@ app.route("/template")
+def template():
+    return render_template("template.html")
+
+@app.route("/About")
+def about():
+    return render_template("About.html")
+
+
+@app.route("/kitchen")
+def kitchen():
+    return render_template("kitchen-compartment.html")
+
+
+@app.route("/left-wing")
+def left():
+    return render_template("left-wing.html")
+
+
+@app.route("/office")
+def office():
+    return render_template("office.html")
+
+
+@app.route("/right-wing")
+def right():
+    return render_template("right-wing.html")
+
+
+@app.route("/bikini-bottom")
+def bikini():
+    return render_template("bikini-bottom.html")
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/style.js
@@ -0,0 +1,24 @@
+let room = document.querySelectorAll(".room");
+
+room.forEach(item => {
+    item.addEventListener('mouseover', event => {
+        event.target.style.fill = "blue";
+        event.target.style.opacity = "0.3";
+        event.target.style.pointerEvents = "all";
+        event.target.style.cursor = "pointer";
+        event.target.style.strokeOpacity = "1";
+    })
+})
+
+
+room.forEach(item => {
+    item.addEventListener('mouseout', event => {
+        event.target.style.fill = "none";
+        event.target.style.stroke = "black";
+        event.target.style.pointerEvents = "all";
+        event.target.style.strokeOpacity = "1";
+        event.target.style.fillOpacity = "0.5";
+    })
+})
+
+
