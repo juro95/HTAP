b3761114dac92295d70988ae25bc2fa03cace894
Julian Römer <julian_romer@orange.fr>
2021-04-25 11:58:48 +0200

    testing availability in compartement 1

--- static/js/app.js
+++ static/js/app.js
@@ -42,6 +42,43 @@ let rooms = {
     "Rock": "c_1886i0393ltc8gqgmach00hf1odhg@resource.calendar.google.com"
 }
 
+let comp_1 = {
+    "Ada": "code.berlin_188ff8i403g5ajughddn43j69rl166gb6oo38e9g74s3gchp60@resource.calendar.google.com",
+    "Echo": "code.berlin_188f0qjk7i3s8g6bj3dtntl59ppd46gb68r34c9l64p3achp60@resource.calendar.google.com",
+    "Warp": "code.berlin_188f2u4uqje8uh9sh50ell5nlacva6gb6op34d9l6sq34chk6g@resource.calendar.google.com",
+    "Zuse": "code.berlin_1888rqjtdkh70i80n4v45e7km81ua6gb68pjae1k74qjcdhk68@resource.calendar.google.com",
+    "8-Bit": "code.berlin_188f0qjk7i3s8g6bj3dtntl59ppd46gb68r34c9l64p3achp60@resource.calendar.google.com",
+    "Rock": "c_1886i0393ltc8gqgmach00hf1odhg@resource.calendar.google.com",
+    "Paper": "code.berlin_3635313131353437333332@resource.calendar.google.com",
+}
+
+let comp_2 = {
+    "Lizard": "code.berlin_1885dv1guu8e4gsdmna2vusmqgqh2@resource.calendar.google.com",
+    "Lizard": "code.berlin_1885dv1guu8e4gsdmna2vusmqgqh2@resource.calendar.google.com",
+    "Spock": "code.berlin_3337333133353032313436@resource.calendar.google.com"
+}
+
+let comp_3 = {
+    "Jungle": "code.berlin_188b81q8a620qia5jnkdohp7bu0ug6g96ss32d1i6cq3ae0@resource.calendar.google.com",
+    "Morty": "code.berlin_31333137303136343636@resource.calendar.google.com",
+    "Rick": "code.berlin_3736373335323835363837@resource.calendar.google.com",
+    "Peace": "code.berlin_1886llujif4fmjgpiogj4n145vi0a6gb74o30d9h6kpjee9k6c@resource.calendar.google.com",
+    "Roomy": "code.berlin_3135303639333334323335@resource.calendar.google.com"
+}
+
+let comp_4 = {
+    "Void-Lab": "c_188d0sd2j8pnei5ai0f0ijl0p3bmg4gbcdnm8p9ec9in4r39do@resource.calendar.google.com",
+    "R2": "code.berlin_1885vk34enieoitrk3b4l5vottl5m@resource.calendar.google.com",
+    "D-2": "code.berlin_1883j5g4liq5ihuehfm64pgo3o66g@resource.calendar.google.com",
+}
+
+let comp_5 = {
+    "Mr. Krabs": "c_1888es6bfll5kjsuls02f06lf7vbs4gbcdnm8p9ec9in4r39do@resource.calendar.google.com",
+    "Plankton": "c_1880o8abbdc0iho8k006b8p16m8p64gbcdnm8p9ec9in4r39do@resource.calendar.google.com",
+    "Spongebob": "c_188c5qoo8kuh2jkdnfusmkfc4hcuc4gbcdnm8p9ec9in4r39do@resource.calendar.google.com",
+    "Squidward": "c_1883gns7qtrjsgvlj179giafkblua4gbcdnm8p9ec9in4r39do@resource.calendar.google.com",
+    "Patrick": "c_1880fd3bis93ehhvglb5b5kb9llle4gbcdnm8p9ec9in4r39do@resource.calendar.google.com",
+}
 
 
 
@@ -224,9 +261,9 @@ function avalabilityCheck() {
             input.addEventListener('change', function () {
                     if (date.value !== "" && startTime.value !== "" && endTime.value !== ""
                     ) {
-                        for (let key in rooms) {
-                            if (rooms.hasOwnProperty(key)) {
-                                let calendarID = rooms[key];
+                        for (let key in comp_1) {
+                            if (comp_1.hasOwnProperty(key)) {
+                                let calendarID = comp_1[key];
                                 let roomName = key;
                                 //console.log(value);
                         //user input that goes into the freebusy query
@@ -249,7 +286,17 @@ function avalabilityCheck() {
                                 console.log(responseObject);
                                 if (resp.calendars[calendarID].busy.length < 1) {
                                     console.log(`${roomName} is free`);
-                                } else if (resp.calendars[calendarID].busy.length === 1) {
+                                    document.querySelector("#comp-one").style.color = "green";
+                                    document.querySelector("#comp-one").style.opacity = "0.3";
+                                }
+                                else {
+                                    document.querySelector("#comp-one").style.color = "red";
+                                    document.querySelector("#comp-one").style.opacity = "0.3";
+                                }
+
+
+                                /**
+                                else if (resp.calendars[calendarID].busy.length === 1) {
                                     console.log(resp.calendars[calendarID].busy[0].start);
                                     console.log(resp.calendars[calendarID].busy[0].end);
                                 } else if (resp.calendars[calendarID].busy.length === 2) {
@@ -274,7 +321,7 @@ function avalabilityCheck() {
                                     console.log(resp.calendars[calendarID].busy[3].start);
                                     console.log(resp.calendars[calendarID].busy[3].end);
 
-                                }
+                                }*/
                             })
                     }}
                     } else {
